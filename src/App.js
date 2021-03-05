214f43e91f0bd22be6d69ad6ed173067dacc5ec5
Asfakur <saikat.hap@gmail.com>
2021-03-05 16:21:50 +0600

    data load with axios practice

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
 import { Button } from '@material-ui/core';
+import axios from 'axios';
 import { useEffect, useState } from 'react';
 import './App.css';
 import News from './components/News/News';
 
 function App() {
   const [articles, setArticles] = useState([]);
+  // useEffect( () => {
+  //   const url = 'https://newsapi.org/v2/top-headlines?country=us&apiKey=65a1a4d3d5f3486194cddbcf72195b41';
+  //   fetch(url)
+  //   .then(res => res.json())
+  //   .then(data =>setArticles(data.articles))
+  // },  [])
+
   useEffect( () => {
     const url = 'https://newsapi.org/v2/top-headlines?country=us&apiKey=65a1a4d3d5f3486194cddbcf72195b41';
-    fetch(url)
-    .then(res => res.json())
-    .then(data =>setArticles(data.articles))
-  },  [])
+    axios(url)
+    .then(data => setArticles(data.data.articles))
+  }, [])
   return (
     <div>
       <h2>Headlines {articles.length}</h2>
